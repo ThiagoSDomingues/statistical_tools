1bb8086d95171c8e3a2a95c9af5e06ce3332a302
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-01-03 21:50:39 -0300

    Add author and docstring to HeavyIonExpt.py
    Added author information and docstring to the script.

--- Heavy-ions/HeavyIonExpt.py
+++ Heavy-ions/HeavyIonExpt.py
@@ -1,1 +1,11 @@
+### Author: OptimustThi
+"""
+This is a script to handle experimental data for heavy-ion collisions. It must downloads, processes, and stores.
+"""
 
+import pickle
+import numpy as np
+
+
+if __name__ == '__main__':
+    print_data(data)
