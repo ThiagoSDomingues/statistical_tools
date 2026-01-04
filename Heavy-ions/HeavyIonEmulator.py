032a8a621e53badb8ebe63e6c00ff629b5b2263b
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-01-03 21:46:57 -0300

    Add author and description to HeavyIonEmulator.py
    Added author information and a brief description of the script.

--- Heavy-ions/HeavyIonEmulator.py
+++ Heavy-ions/HeavyIonEmulator.py
@@ -1,1 +1,22 @@
+### Author: OptimusThi
+"""
+Script to train Gaussian emualators.
+"""
+import pickle
+import numpy as np
+from sklearn.gaussian_process import GaussianProcessRegressor as GPR
+from sklearn.gaussian_process import kernels
 
+
+# Unfinished!
+def predict(self, X, return_cov=False, extra_std=0):
+    """
+    Predict model output at a point in parameter space `X`. 
+    """
+    if return_cov:
+        return mean, _Covariance(cov, self._slices)
+    else:
+        return mean
+
+if __name__ == '__main__:
+  
