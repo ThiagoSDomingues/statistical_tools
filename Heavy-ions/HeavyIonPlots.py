1c97a98e8bc6fd9ee53eeb7617b0c1f502f66a32
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-01-05 23:51:39 -0300

    Implement plot_prior_CI function for predictions
    Added functionality to plot prior and Bayesian design point predictions with credible intervals.

--- Heavy-ions/HeavyIonPlots.py
+++ Heavy-ions/HeavyIonPlots.py
@@ -10,5 +10,34 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
+from HeavyIonPCA import inverse_tf_matrix, scaler
+from HeavyIonEmulators import predict_observables 
 from HeavyIonMCMC import Chain
-from HeavyIonEmulators import predictions 
+### Load: Emulators, inverse_tf_matrix, scaler
+
+@plot
+def plot_prior_CI(
+    n_pr_samples, 
+    show_exp=False,
+    bound_min=5,
+    bound_max=95,
+    save_folder="prior"
+):
+    """
+    Plot prior and Bayesian design point predictions overlaid with 90% credible intervals.
+    """
+  
+    # Collect scaled spectra predictions for each prior sample
+    pr_predictions = []
+
+    n_params = 17 # number of parameters (parameter space dimension)
+    
+    # Looping over all prior samples: supposing a uniform prior
+    # Use the emulator surrogate to predict observables for each prior sample
+    for params in np.random.uniform(design_min, design_max, (n_pr_samples, n_params)):
+        y_pred, cov_pred = predict_observables(params, Emulators, inverse_tf_matrix, scaler)
+        pr_predictions.append(y_pred.flatten())
+    
+    pr_predictions = np.array(pr_predictions) # shape (n_samples, n_obs)
+     
+    
