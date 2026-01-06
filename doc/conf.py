44bfebdb272204e51e4eb15f39eb7ea9da9ec631
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-01-06 15:01:33 -0300

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -1,1 +1,12 @@
+# -*- coding: utf-8 -*-
 
+import datetime
+import importlib
+import inspect
+import os 
+import sys
+
+project = 'statistical_tools'
+version = realease = ''
+author = 'OptimusThi'
+copyright = '{} {}'.format(datetine.date.today().year, author) 
