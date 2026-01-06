59cfa44910fba12e97d3c439bceaaa9778c693ad
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-01-05 23:36:06 -0300

    Add shebang and docstring to HeavyIonPlots.py
    Added a shebang and docstring to the script.

--- Heavy-ions/HeavyIonPlots.py
+++ Heavy-ions/HeavyIonPlots.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python3
 
+"""
+This script generates plots / figures for Bayesian analysis results in heavy-ion collisions
+"""
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+from HeavyIonMCMC import Chain
+from HeavyIonEmulators import predictions 
