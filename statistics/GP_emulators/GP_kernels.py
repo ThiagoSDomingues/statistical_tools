36834759a62be5f31e126837591898d5557e8838
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-01-18 20:17:23 -0300

    Create GP_kernels.py

---
+++ statistics/GP_emulators/GP_kernels.py
@@ -0,0 +1,7 @@
+### Author: OptimusThi
+"""
+Script to specify and compare different kernels for GPR
+"""
+
+from sklearn.gaussian_process import GaussianProcessRegressor  
+from sklearn.gaussian_process.kernels import RBF, WhiteKernel
