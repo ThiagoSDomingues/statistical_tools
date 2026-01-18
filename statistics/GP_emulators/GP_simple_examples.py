1b1e520debe045b189f365bc0219cc5c1d6d3cdc
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-01-18 15:54:50 -0300

    Update GP_simple_examples.py

--- statistics/GP_emulators/GP_simple_examples.py
+++ statistics/GP_emulators/GP_simple_examples.py
@@ -3,4 +3,15 @@
 """
 script to use a Gaussian process regression to fit heavy-ion collisions experimental data
 """
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF
+
+# load experimental data (without uncertainties)
+
+# fitting our model: initialization of hyperparameters
+kernel = 1 * RBF(length_scale=1.0, length_scale_bounds=(1e-2, 1e2))
+gaussian_process
+gaussian_process.fit()
+gaussian_process.kernel_ 
+
 
