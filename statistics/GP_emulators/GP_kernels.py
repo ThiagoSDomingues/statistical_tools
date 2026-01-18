4a9952c2526f292808d7b0b55e45ee3fc31581b1
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-01-18 20:19:19 -0300

    Update GP_kernels.py

--- statistics/GP_emulators/GP_kernels.py
+++ statistics/GP_emulators/GP_kernels.py
@@ -5,3 +5,9 @@ Script to specify and compare different kernels for GPR
 
 from sklearn.gaussian_process import GaussianProcessRegressor  
 from sklearn.gaussian_process.kernels import RBF, WhiteKernel
+
+kernel = 1.0 * RBF(length_scale=1e1, length_scale_bounds=(1e-2, 1e3)) + WhiteKernel(noise_level=1, noise_level_bounds=(1e-10, 1e1))
+gpr = GaussianProcessRegressor(kernel=kernel, alpha=0.0)
+gpr.fit(X_train, y_train)
+y_mean, y_std = gpr.predict(X, return_std=True)
+
