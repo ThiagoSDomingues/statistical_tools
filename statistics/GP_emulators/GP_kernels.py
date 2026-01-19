c93609885d1374b2ab0ad4d6f4ac61f923a270b9
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-01-19 14:01:12 -0300

    Fix training data variable names in GP_kernels.py

--- statistics/GP_emulators/GP_kernels.py
+++ statistics/GP_emulators/GP_kernels.py
@@ -7,6 +7,8 @@ import numpy as np
 from sklearn.gaussian_process import GaussianProcessRegressor  
 from sklearn.gaussian_process.kernels import RBF, WhiteKernel, Matern
 
+X_train, y_train = load_training_data
+
 kernel = 1.0 * RBF(length_scale=1e1, length_scale_bounds=(1e-2, 1e3)) + WhiteKernel(noise_level=1, noise_level_bounds=(1e-10, 1e1))
 gpr = GaussianProcessRegressor(kernel=kernel, alpha=0.0)
 gpr.fit(X_train, y_train)
@@ -18,5 +20,5 @@ kernel = Matern(length_scale=1.0, nu=1.5)
 
 gp = GaussianProcessRegressor(kernel=kernel, optimizer='fmin_l_bfgs_b', n_restarts_optimizer=10, normalize_y=True)
 
-gp.fit(X, y)
+gp.fit(X_train, y_train)
 
