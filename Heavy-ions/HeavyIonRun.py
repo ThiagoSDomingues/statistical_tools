28a4dd6466c68fc212b81a19bc94e86ad350fbe8
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-01-18 22:57:37 -0300

    Enhance output with initialization and metrics prints
    Added print statements for initialization and validation metrics.

--- Heavy-ions/HeavyIonRun.py
+++ Heavy-ions/HeavyIonRun.py
@@ -5,4 +5,16 @@ Script to run all the Bayesian analysis pipeline
 from HeavyIonDesign.py import * 
 from HeavyIonPCA.py import * 
 
+print("=" * 70)
+print("Initializing: sampling the parameter space -- Generating design points")
+print("=" * 70)
+
+print("=" * 70)
+print("GP EMULATOR FRAMEWORK: FITTING A GPR")
+print("=" * 70)
+
+print(f"\nDataset:")
+
+print(f"\nValidation metrics:")
+print(metrics)
 
